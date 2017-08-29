78c9ab188438a9673b5823f891d71053d196d519
Salvio Furbino Elias <salviof@gmail.com>
2017-08-29 18:35:20 -0300

    Atualizacao automática

--- webApp/src/main/webapp/resources/SBComp/recursos/js/enterTabulacao.js
+++ webApp/src/main/webapp/resources/SBComp/recursos/js/enterTabulacao.js
@@ -18,20 +18,23 @@ $(document).keydown(function (e) {
         if (e.which === 13 && !self.is('textarea,div[contenteditable=true]')) { // [Enter] key
 
 
-            if (!self.is('button', 'a')) {
+            if (!self.is('button[type="submit"]')) {
+                if (!self.is('button', 'a')) {
 
 
-                // If not a regular hyperlink/button/textarea
-                if ($.inArray(self, focusable)) {
-                    // Then prevent the default [Enter] key behaviour from submitting the form
-                    e.preventDefault();
-                } // Otherwise follow the link/button as by design, or put new line in textarea
+                    // If not a regular hyperlink/button/textarea
+                    if ($.inArray(self, focusable)) {
+                        // Then prevent the default [Enter] key behaviour from submitting the form
+                        e.preventDefault();
+                    } // Otherwise follow the link/button as by design, or put new line in textarea
+                }
+
+                // Focus on the next item (either previous or next depending on shift)
+                focusable.eq(focusable.index(self) + (e.shiftKey ? -1 : 1)).focus();
             }
-
-            // Focus on the next item (either previous or next depending on shift)
-            focusable.eq(focusable.index(self) + (e.shiftKey ? -1 : 1)).focus();
-
             return false;
+
+
         }
     }
     // We need to capture the [Shift] key and check the [Enter] key either way.
